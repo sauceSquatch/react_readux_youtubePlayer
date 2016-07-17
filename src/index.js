5469180070ea0c599ad785169edc288eac247fa9
Jamie Naioti <jnaioti@usausrad8277.global.publicisgroupe.net>
2016-07-17 08:40:36 -0500

    moved the YTSearch function into the constructor of App and set the results to the prop 'videos' on state to make available to the other components of app. Created videoList component that gets fed data through props, right now just exporting basic data

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js';
 
 const API_KEY = 'AIzaSyChoVyW8YdDpZG7TQ35hqReIu8wfvKLnrg';
 
-YTSearch( {key: API_KEY, term: 'SURF TIME'}, function(data) {
-  console.log(data);
-});
-
 /// create a new component that produces HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch( {key: API_KEY, term: 'SURF TIME'}, (data) => {
+      this.setState({ videos: data });
+      console.log("data", this.state.videos);
+    });
+
+    //  in ES6 the data key and property can be named the same and condensed as:
+    // YTSearch( {key: API_KEY, term: 'SURF TIME'}, (videos) => {
+    //   this.setState({ videos });
+    // });
+    // I'm going to leave verbose for learning 
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // make sure the component gets added to the DOM
