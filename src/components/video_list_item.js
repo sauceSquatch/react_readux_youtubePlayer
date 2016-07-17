e0d11317eb8e81f71df7972c1702562e8eda5ede
Jamie Naioti <jnaioti@usausrad8277.global.publicisgroupe.net>
2016-07-17 09:34:44 -0500

    creating video var in VideoListItems with the video object being passed from props and console.log that out

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+// this is the verbose way to set video prop
+// const VideoListItem = (props) => {
+//   const video = props.video;
+  
+//   return <li>video</li>
+// }
+
+
+// ES6 way, by doing this it's saying there is a props.video property, please make a new var called the same name video
+const VideoListItem = ({video}) => {
+  console.log(video);
   return <li>video</li>
 }
 
